a5a43a687145b472b0205839f384a000050d88d0
zhwbqd <zhwbqd@gmail.com>
2013-05-18 13:34:30 +0800

    update

--- src/main/java/zhwb/product/largesort/util/MemoryMonitor.java
+++ src/main/java/zhwb/product/largesort/util/MemoryMonitor.java
@@ -1,10 +1,3 @@
-/*
- * Copyright Notice ====================================================
- * This file contains proprietary information of Hewlett-Packard Co.
- * Copying or reproduction without prior written approval is prohibited.
- * Copyright (c) 2012 All rights reserved. =============================
- */
-
 package zhwb.product.largesort.util;
 
 import org.slf4j.Logger;
